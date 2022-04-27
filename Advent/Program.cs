d57fefe387261876d76be56f817d965c0e912c2f
Niklas Larsson <niklaslarsson95@gmail.com>
2022-04-27 21:39:38 +1000

    Add total millis to output

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -7,14 +7,17 @@ var days = new Day[] { new Day1("2021/input1"), new Day2("2021/input2"), new Day
 Console.WriteLine($"+-----+---+---+-----------------+");
 Console.WriteLine($"| Day | 1 | 2 | Solve Time (ms) |");
 Console.WriteLine($"+-----+---+---+-----------------+");
+var totalMillis = 0.0;
 foreach(Day day in days)
 {
     var start = DateTime.Now;
     day.Solve();
     var end = DateTime.Now;
     var solveTime = end - start;
+    totalMillis += solveTime.TotalMilliseconds;
     Console.WriteLine($"|  {day.DayName} | {day.Valid1} | {day.Valid2} | {solveTime.TotalMilliseconds:       00000.00} |");
 }
 Console.WriteLine($"+-----+----------+--------------+");
 
+Console.WriteLine($"  Total solve time {totalMillis:0.00} ms");
 
